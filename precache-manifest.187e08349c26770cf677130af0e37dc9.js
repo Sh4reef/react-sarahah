f53895ae1860f314fcb7712beedd124075ba8cb6
Sh4reef <shareef.banjar@gmail.com>
2019-04-08 01:33:49 +0800

    Updates

---
+++ precache-manifest.187e08349c26770cf677130af0e37dc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "436a5e6430c17a8b567e",
+    "url": "/sarahah/static/css/main.f4468e9d.chunk.css"
+  },
+  {
+    "revision": "436a5e6430c17a8b567e",
+    "url": "/sarahah/static/js/main.436a5e64.chunk.js"
+  },
+  {
+    "revision": "c6971b9daa4cd45339c3",
+    "url": "/sarahah/static/js/1.c6971b9d.chunk.js"
+  },
+  {
+    "revision": "9e3c9015b7f3183aaeca",
+    "url": "/sarahah/static/js/runtime~main.9e3c9015.js"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/sarahah/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/sarahah/static/media/bg7.013b0488.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/sarahah/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "d57fa885fa5c7002dc43e9dd61172af7",
+    "url": "/sarahah/static/media/office2.d57fa885.jpg"
+  },
+  {
+    "revision": "b23f720528371939315c0df5fc90c150",
+    "url": "/sarahah/static/media/christian.b23f7205.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/sarahah/static/media/city.58a713c7.jpg"
+  },
+  {
+    "revision": "f527b8c20f163c72068151a7af176ae7",
+    "url": "/sarahah/index.html"
+  }
+];
